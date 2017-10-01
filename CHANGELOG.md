933b714b7f7442f7c1c238f00a83f7f30ae69b63
raoulvdberge <raoulvdberge@gmail.com>
2017-10-01 14:00:00 +0200

    Bump to 0.2 and to MC 1.12.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Refined Storage Addons Changelog
 
+### 0.2
+- Updated Forge to 2493 (MC 1.12.2) (raoulvdberge)
+
 ### 0.1.1
 - Refined Storage 1.5.13 compatibility (raoulvdberge)
 - Added Chinese translation (TartaricAcid)
--- src/main/java/com/raoulvdberge/refinedstorageaddons/RSAddons.java
+++ src/main/java/com/raoulvdberge/refinedstorageaddons/RSAddons.java
@@ -12,14 +12,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = RSAddons.ID, version = RSAddons.VERSION, dependencies = RSAddons.DEPENDENCIES, guiFactory = RSAddons.GUI_FACTORY)
+@Mod(modid = RSAddons.ID, version = RSAddons.VERSION, dependencies = RSAddons.DEPENDENCIES, acceptedMinecraftVersions = "[1.12,1.13)", guiFactory = RSAddons.GUI_FACTORY)
 public final class RSAddons {
     @RSAPIInject
     public static IRSAPI RSAPI;
 
     public static final String ID = "refinedstorageaddons";
-    public static final String VERSION = "0.1.1";
-    public static final String DEPENDENCIES = "required-after:refinedstorage@[1.5.13,);";
+    public static final String VERSION = "0.2";
+    public static final String DEPENDENCIES = "required-after:refinedstorage@[1.5.19,);";
     public static final String GUI_FACTORY = "com.raoulvdberge.refinedstorageaddons.gui.config.ModGuiFactory";
 
     @Mod.Instance
